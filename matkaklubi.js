a69bf371a5bcc1245ebca785122f6ff6ab6a2282
Andres Järviste <andres.jarviste@gmail.com>
2020-05-21 16:58:44 +0300

    algseis

---
+++ matkaklubi.js
@@ -0,0 +1,55 @@
+function kuupaevStringina(date) {
+    return  date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+}
+
+function testRida(matk) {
+    return (
+        '<div class="col-lg-4 col-md-12 matkarida">' + 
+            '<div class="card">' +
+                '<img src="' + matk.pilt + '" class="img-fluid matkarida-pilt" alt="">' +
+                '<a href="#">' +
+            '</a>' +
+                '<div class="card-body">' +
+                '    <h4 class="card-title">' + matk.nimi + ': ' +  + '</h4>' +
+                '    <p class="card-text">' + matk.lyhikirjeldus + '</p>' +
+                '    <a href="' + matk.matkaleht + '" class="btn btn-primary">Vaata täpsemalt</a>' +
+                '</div>' +
+            '</div>' +
+        '</div>'
+    );
+}
+
+const matkad = [
+    { 
+        nimi: 'Kanuumatk Võhandul', 
+        kuupaev: new Date(2020, 06, 10), 
+        pilt: './images/matk.png', 
+        matkaleht: 'matk1.html', 
+        lyhikirjeldus: 'qpowieurpqoiweuropiu qwoeiur qowpie rqowie ropqiwu eroqi ewrqlwöe rölqwe rölqkjwe rölqjw eröljqwerö' 
+    },
+    { 
+        nimi: 'Rabamatk Laeva rabas', 
+        kuupaev: new Date(2020, 08, 12), 
+        pilt: './images/golfivaljak.jpg', 
+        matkaleht: 'matk1.html',
+        lyhikirjeldus: 'asödlkfaskhfalksdf lkashdf lkas dflkasdflka sdflka sdflkasdlfhasdlfkjhasld fasdf. Asdlfkjasdlfjasld'  
+    },
+    { 
+        nimi: 'Süstaga ümber Hiiumaa', 
+        kuupaev: new Date(2020, 10, 10), 
+        pilt: './images/systamatk.jpg', 
+        matkaleht: 'matk1.html',
+        lyhikirjeldus: 'öflajsdfölkasdf asdflökjasdölfjasö dlfaslödf. afaölksdjföalkjsdf löasdfölkjasd flökjasdf' 
+    },
+];
+
+let matk = '';
+
+for (i = 0; i < matkad.length; i++) {
+    const tana = new Date();
+    if (tana < matkad[i].kuupaev) {
+        matk += testRida(matkad[i]); //matk = matkarida(matkad[i])
+    }
+}
+
+document.getElementById("matkaandmed").innerHTML = matk;
