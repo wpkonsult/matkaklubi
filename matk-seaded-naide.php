6efbd6cd4fda2395b20a72c6c28a8b2b79bcf620
Andres Järviste <andresjarviste@gmail.com>
2020-06-16 23:05:23 +0300

    saladusi ei hoia githubis

---
+++ matk-seaded-naide.php
@@ -0,0 +1,9 @@
+<?php
+function annaSaladused() {
+    return [
+        "servername" => "matka.serveri.nimi",
+        "username" => "sinu_kasutaja",
+        "password" => "salas6na",
+        "dbname" => "matka_andmebaas",
+    ];
+}
