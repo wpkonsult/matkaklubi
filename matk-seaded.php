02e08d73ebf6b778cbaa5e6a2be7c751e0649138
Andres Järviste <andres.jarviste@gmail.com>
2020-06-16 23:59:25 +0300

    parandus

--- matk-seaded.php
+++
@@ -1,9 +0,0 @@
-<?php
-function annaSaladused() {
-    return [
-        "servername" => "d55765.mysql.zonevs.eu",
-        "username" => "d55765_matkaklub",
-        "password" => "p6randaLamp7",
-        "dbname" => "d55765_matkaklubi",
-    ];
-}
